49dc23cbc5d8fa8f58dfda2a4213ead4a37ac901
etochy <etochy@gmail.com>
2017-11-02 16:22:42 +0100

    Comment VenteImpl

--- Enchere/src/serveur/VenteImpl.java
+++ Enchere/src/serveur/VenteImpl.java
@@ -13,7 +13,7 @@ import java.util.Stack;
 import client.IAcheteur;
 
 public class VenteImpl extends UnicastRemoteObject implements IVente{
-	
+
 	private static final long serialVersionUID = 1L;
 	private List<IAcheteur> listeAcheteurs = new ArrayList<IAcheteur>();
 	private List<IAcheteur> fileAttente = new ArrayList<IAcheteur>();
@@ -23,13 +23,13 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 	private IAcheteur acheteurCourant;
 	private EtatVente etatVente;
 	private final int clientMin = 2;
-	
-	
+
+
 	protected VenteImpl() throws RemoteException {
 		super();
 		this.etatVente = EtatVente.ATTENTE;
 	}
-	
+
 	public VenteImpl(Stack<Objet> listeObjets) throws RemoteException {
 		super();
 		this.listeObjets = listeObjets;
@@ -49,7 +49,7 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 
 		if(this.fileAttente.size() >= clientMin && this.etatVente == EtatVente.ATTENTE){
 			this.etatVente = EtatVente.ENCHERISSEMENT;	
-			
+
 			for(IAcheteur each : this.fileAttente){
 				this.listeAcheteurs.add(each);
 				each.objetVendu(null);
@@ -60,12 +60,12 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 		return false;
 	}
 
-	
+
 	@Override
 	public synchronized int rencherir(int nouveauPrix, IAcheteur acheteur) throws Exception{
 		this.enchereTemp.put(acheteur, nouveauPrix);
 		System.out.println(this.enchereTemp.size()+"/"+this.listeAcheteurs.size());
-		
+
 		//On a recu toutes les encheres
 		if(this.enchereTemp.size() == this.listeAcheteurs.size()){
 			if(enchereFinie()){
@@ -82,7 +82,7 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 		return objetCourant.getPrixCourant();
 	}
 
-	
+
 	/**
 	 * Permet de passer à l'objet suivant avec les bons acheteurs et bons objets.
 	 * @throws RemoteException
@@ -91,19 +91,19 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 	public int objetSuivant() throws RemoteException, InterruptedException{
 		this.enchereTemp.clear();
 		this.etatVente = EtatVente.ATTENTE;
-		
+
 		if(acheteurCourant != null){
 			this.objetCourant.setDisponible(false);
 			this.objetCourant.setGagnant(this.acheteurCourant.getPseudo());
-			
+
 			//Envoie des resultats finaux pour l'objet courant
 			for(IAcheteur each : this.listeAcheteurs){
 				each.objetVendu(this.acheteurCourant.getPseudo());
-				
+
 			}
 		}
-		
-		
+
+
 
 		Thread.sleep(5000);
 		this.acheteurCourant = null;
@@ -127,9 +127,9 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 		}
 		return this.objetCourant.getPrixBase();
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Methode utilitaire permettant d'actualiser le prix de l'objet et le gagnant selon les encheres recues.
 	 * @throws RemoteException
@@ -137,21 +137,21 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 	public void actualiserObjet() throws RemoteException{
 		Set<IAcheteur> cles = this.enchereTemp.keySet();
 		Iterator<IAcheteur> it = cles.iterator();
-		
+
 		while (it.hasNext()){
 			IAcheteur cle = it.next();
 			Integer valeur = this.enchereTemp.get(cle);
-			
+
 			if(valeur > this.objetCourant.getPrixCourant() || (valeur == this.objetCourant.getPrixCourant() && cle.getChrono() < acheteurCourant.getChrono())){
 				this.objetCourant.setPrixCourant(valeur);
 				this.acheteurCourant = cle;	
 				this.objetCourant.setGagnant(this.acheteurCourant.getPseudo());
-		   }
+			}
 		}
 		this.enchereTemp.clear();
 	}
-	
-	
+
+
 	/**
 	 * méthode utilitaire qui permet de savoir si les encheres sont finis.
 	 * @return true si on a reçu que des -1, donc si l'enchere est finie, sinon false.
@@ -159,18 +159,18 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 	public boolean enchereFinie(){	
 		Set<IAcheteur> cles = this.enchereTemp.keySet();
 		Iterator<IAcheteur> it = cles.iterator();
-		
+
 		while (it.hasNext()){
 			IAcheteur cle = it.next();
 			Integer valeur = this.enchereTemp.get(cle);
-			
+
 			if(valeur != -1){
 				return false;	
-		   }
+			}
 		}
 		return true;
 	}
-	
+
 
 
 	@Override
@@ -180,7 +180,7 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@Override
@@ -227,8 +227,4 @@ public class VenteImpl extends UnicastRemoteObject implements IVente{
 	public void setEtatVente(EtatVente etatVente) {
 		this.etatVente = etatVente;
 	}
-
-	
-
-
 }
